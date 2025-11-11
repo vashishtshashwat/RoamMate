cb569a192895f4fe72303f51cd841fb90d71e1ec
vashishtshashwat <158034077+vashishtshashwat@users.noreply.github.com>
2025-11-11 14:13:34 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -75,7 +75,7 @@ app.use((err, req, res, next) => {
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://roam-mate.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true
   }
